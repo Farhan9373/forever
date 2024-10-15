44b2a44b4ce21b43134d2ce4686326580fc60afe
Farhan <farhanahmadptn@gmail.com>
2024-10-15 11:58:59 +0530

    index.js changes

--- backened/index.js
+++ backened/index.js
@@ -17,7 +17,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use("/api",router);
 
-const PORT=8000||process.env.PORT
+const PORT = process.env.PORT || 8000;
+
 
 ConnectDB().then(()=>{
     app.listen(PORT,()=>{
