703cc1466faddecf26f9c5b68371f54bd7042c83
Farhan <farhanahmadptn@gmail.com>
2024-10-15 12:24:37 +0530

    db.js changes

--- backened/config/DB.js
+++ backened/config/DB.js
@@ -1,24 +1,13 @@
 import mongoose from "mongoose";
 
-let isConnected; // track the connection state
-
-async function ConnectDB() {
-    if (isConnected) {
-        return; // use existing connection
-    }
-
+async function ConnectDB (){
     try {
-        // Connect to MongoDB
-        await mongoose.connect(process.env.MONGODB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        isConnected = true; // set the connection state to true
-        console.log("MongoDB connected successfully.");
-    } catch (error) {
-        console.error("MongoDB connection error:", error);
-        throw new Error("Failed to connect to MongoDB");
+        mongoose.connect(process.env.MONGODB_URL)
+
+    }   
+    catch (error) {
+        console.log(error);
+        
     }
 }
-
-export default ConnectDB;
+export default ConnectDB
