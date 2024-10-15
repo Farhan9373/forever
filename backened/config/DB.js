573749906804231223d7eab7424a0a6fe69155af
Farhan <farhanahmadptn@gmail.com>
2024-10-15 12:20:42 +0530

    db.js changes

--- backened/config/DB.js
+++ backened/config/DB.js
@@ -1,13 +1,24 @@
 import mongoose from "mongoose";
 
-async function ConnectDB (){
-    try {
-        mongoose.connect(process.env.MONGODB_URL)
+let isConnected; // track the connection state
 
-    }   
-    catch (error) {
-        console.log(error);
-        
+async function ConnectDB() {
+    if (isConnected) {
+        return; // use existing connection
+    }
+
+    try {
+        // Connect to MongoDB
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        isConnected = true; // set the connection state to true
+        console.log("MongoDB connected successfully.");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        throw new Error("Failed to connect to MongoDB");
     }
 }
-export default ConnectDB
+
+export default ConnectDB;
--- backened/index.js
+++ backened/index.js
@@ -12,6 +12,8 @@ app.use(cors({
     origin:process.env.FRONTEND_URL,
     credentials:true
 }));
+app.options('*', cors());  // Handle preflight CORS requests
+
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
